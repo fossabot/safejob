56fcc1f6c6dadbb75ee94d97bc5a5ed879b4d2b8
Evgenii Lobodin <e.lobodin@semrush.com>
2019-03-27 11:40:18 +0300

    added DoWithContext

--- safejob.go
+++ safejob.go
@@ -6,32 +6,57 @@ import (
 	"sync"
 )
 
+// ErrClosed returns when safejob is closed
 var ErrClosed = errors.New("unsuccessful attempt to perform work after stop")
 
-type safejob struct {
-	closer func()
-	tokens chan func()
-}
+type (
+	safejob struct {
+		closer func()
+		tokens chan *token
+	}
 
+	token struct {
+		context context.Context
+		done    func()
+	}
+)
+
+// New create new instance safejob
 func New(closer func()) *safejob {
 	return &safejob{
 		closer: closer,
-		tokens: make(chan func()),
+		tokens: make(chan *token),
 	}
 }
 
+// Do safe job
 func (s *safejob) Do(f func() error) error {
 	token := <-s.tokens
 	if token == nil {
 		return ErrClosed
 	}
-	defer token()
+	defer token.done()
 	return f()
 }
 
+// Do safe job with context
+func (s *safejob) DoWithContext(f func(context.Context) error) error {
+	token := <-s.tokens
+	if token == nil {
+		return ErrClosed
+	}
+	defer token.done()
+	return f(token.context)
+}
+
+// Run worker
 func (s *safejob) Run(ctx context.Context) {
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
+	t := &token{
+		context: ctx,
+		done:    wg.Done,
+	}
 	for {
 		select {
 		case <-ctx.Done():
@@ -42,7 +67,7 @@ func (s *safejob) Run(ctx context.Context) {
 				s.closer()
 			}
 			return
-		case s.tokens <- wg.Done:
+		case s.tokens <- t:
 			wg.Add(1)
 		}
 	}
--- safejob_test.go
+++ safejob_test.go
@@ -17,6 +17,7 @@ func TestCloseChannel(t *testing.T) {
 		close(ch)
 	})
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	go sj.Run(ctx)
 
 	// prepare jobs
@@ -46,7 +47,7 @@ func TestCloseChannel(t *testing.T) {
 	n := 0
 	rand.Seed(time.Now().Unix())
 	stop := rand.Intn(jobCount * steps / 2)
-	for _ = range ch {
+	for range ch {
 		n++
 		if n == stop {
 			cancel()
@@ -54,4 +55,60 @@ func TestCloseChannel(t *testing.T) {
 	}
 	wg.Wait()
 	assert.EqualValues(t, jobCount*steps, n+int(errCount))
+}
+
+func TestWithContext(t *testing.T) {
+	ch := make(chan struct{}, 1000)
+	sj := New(func() {
+		close(ch)
+	})
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go sj.Run(ctx)
+
+	// prepare jobs
+	var errCount uint64
+	jobCount := 1000
+	steps := 1000
+	start := make(chan struct{})
+	wg := new(sync.WaitGroup)
+	wg.Add(jobCount)
+	for n := 0; n < jobCount; n++ {
+		go func() {
+			defer wg.Done()
+			<-start
+			for i := 0; i < steps; i++ {
+				if err := sj.DoWithContext(func(c context.Context) error {
+					select {
+					case <-c.Done():
+						return c.Err()
+					default:
+					}
+					ch <- struct{}{}
+					<-c.Done()
+					return nil
+				}); err != nil {
+					atomic.AddUint64(&errCount, 1)
+				}
+			}
+		}()
+	}
+	close(start)
+
+	// random terminate
+	n := 0
+	rand.Seed(time.Now().Unix())
+	stop := rand.Intn(jobCount)
+	if stop == 0 {
+		stop = 1
+	}
+	for range ch {
+		n++
+		if n == stop {
+			cancel()
+		}
+	}
+	wg.Wait()
+	assert.EqualValues(t, jobCount*steps, n+int(errCount))
+	assert.True(t, n <= jobCount+1)
 }
